6dfd6b48473ed8c7b478b42d795ac4e5077232c1
Gresham24 <tembogresham@gmail.com>
2023-08-01 01:15:18 +0200

    importing firestore module

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,8 +21,7 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-// Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
 
-
-export default auth;
+export {db, auth};
