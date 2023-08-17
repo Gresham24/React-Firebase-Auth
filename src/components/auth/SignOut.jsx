9b2904f0d4426199b4ec913168cc6ffd94b291cb
Gresham24 <tembogresham@gmail.com>
2023-08-17 23:04:42 +0200

    dashboard redirect

--- src/components/auth/SignOut.jsx
+++ src/components/auth/SignOut.jsx
@@ -1,19 +1,24 @@
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 
 
 const LogoutButton = () => {
+  const navigate = useNavigate()
     const handleLogout = () => {
         signOut(auth)
             .then(() => {
                 console.log("User logged out");
+                navigate('/')
             })
             .catch((error) => {
                 console.error("Logout Error:", error);
             });
     };
 
-    return <button onClick={handleLogout}>Logout</button>;
+    return (
+        <button onClick={handleLogout}>Sign out</button>
+    );
 };
 
 export default LogoutButton;
