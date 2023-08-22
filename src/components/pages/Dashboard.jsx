321ad8692f8ed418322bacd0d1f9f54204549234
Gresham24 <tembogresham@gmail.com>
2023-08-22 08:51:16 +0200

    fetching user details from Firestore

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,17 +1,49 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../auth/AuthContext";
 import LogoutButton from "../auth/SignOut";
+import { db } from "../../firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 function Dashboard() {
     const { currentUser } = useContext(AuthContext); // use the useContext hook to access the AuthContext
+    const [userDetails, setUserDetails] = useState(null);
+
+    useEffect(() => {
+        if (currentUser) {
+            // Fetch the user details from Firestore using the already initialized db
+            const docRef = doc(db, "Users", currentUser.uid);
+            getDoc(docRef)
+                .then((docSnap) => {
+                    if (docSnap.exists()) {
+                        setUserDetails(docSnap.data());
+                    } else {
+                        console.log("No such document!");
+                    }
+                })
+                .catch((error) => {
+                    console.log("Error getting document:", error);
+                });
+        }
+    }, [currentUser]);
 
     return (
         <>
             <h1>Dashboard</h1>
-            <p>
-                <strong>Email:</strong>{" "}
-                {currentUser ? currentUser.email : "Not logged in"}{" "}
-            </p>
+            {currentUser ? (
+                <p>
+                    <strong>Email:</strong> {currentUser.email}
+                </p>
+            ) : (
+                <p>Loading user details...</p>
+            )}
+            {userDetails ? (
+                <p>
+                    <strong>Name:</strong> {userDetails.firstName}{" "}
+                    {userDetails.lastName}
+                </p>
+            ) : (
+                <p>Loading user details...</p>
+            )}
             <LogoutButton />
         </>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Registration from "./components/auth/Registration.jsx";
 import SignIn from "./components/auth/SignIn.jsx";
-import LogoutButton from "./components/auth/SignOut.jsx";
 import AuthProvider from "./components/auth/AuthContext.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Pages/Homepage.jsx";
