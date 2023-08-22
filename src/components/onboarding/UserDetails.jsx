99830ba637d51d874ee9cacb4a5b6c21962a6480
Gresham24 <tembogresham@gmail.com>
2023-08-22 21:25:00 +0200

    added onboarding 3 of 3 user form

--- src/components/onboarding/UserDetails.jsx
+++ src/components/onboarding/UserDetails.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React from "react";
 import { doc, collection, setDoc } from "firebase/firestore";
-import { db } from '../../firebase';
-import { useNavigate } from 'react-router-dom'
+import { db } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 
 function UserDetails() {
-  return (
-    <div>UserDetails</div>
-  )
+    return (
+        <div className="userDetailsContainer">
+            <h1>Complete profile:</h1>
+            <form>
+                <label htmlFor="companyName"></label>
+                <input
+                    type="text"
+                    id="companyName"
+                    name="companyName"
+                    placeholder="Company Name"
+                />
+                <label htmlFor="phoneNumber"></label>
+                <input
+                    type="number"
+                    id="contact"
+                    name="contact"
+                    placeholder="Contact Number"
+                />
+                <label htmlFor="companyName"></label>
+                <input
+                    type="text"
+                    id="city"
+                    name="city"
+                    placeholder="City"
+                />
+                <button type="submit">Save</button>
+            </form>
+        </div>
+    );
 }
 
-export default UserDetails
+export default UserDetails;
