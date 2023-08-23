8f513a08fa02e63c9dfe36e1a7aa452f3c25ec00
Gresham24 <tembogresham@gmail.com>
2023-08-24 01:44:43 +0200

    onboarding pg 2 form layout

--- src/components/onboarding/About.jsx
+++ src/components/onboarding/About.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { doc, collection, setDoc } from "firebase/firestore";
+import { auth, db } from "../../firebase";
+import { AuthContext } from "../auth/AuthContext";
 
 function About() {
     const navigate = useNavigate();
@@ -9,15 +12,30 @@ function About() {
         navigate("/userdetails");
     };
 
-  return (
-      <>
-          <h1>About</h1>
-          <p>Page 2/3</p>
-          <button type="button" onClick={handleSubmit}>
-              Next
-          </button>
-      </>
-  );
+    return (
+        <>
+            <h1>About</h1>
+            <p>
+                Lorem ipsum, dolor Esse nam sit ratione recusandae perferendis,
+                et id magni odio
+            </p>
+            <div className="aboutContainer">
+                <form>
+                    <label htmlFor="city">City</label>
+                    <input
+                        type="text"
+                        id="city"
+                        name="city"
+                        placeholder="City"
+                        // value={city}
+                        // onChange={(e) => setCity(e.target.value)}
+                    />
+                    <button type="submit">Save</button>
+                </form>
+            </div>
+            <p>Page 2/3</p>
+        </>
+    );
 }
 
-export default About
+export default About;
