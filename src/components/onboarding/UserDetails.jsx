9f9ba76c07a1d4aa315915169a9445b795b0f6ae
Gresham24 <tembogresham@gmail.com>
2023-08-24 02:04:45 +0200

    onboarding page 3 of 3 form and link to dashboard

--- src/components/onboarding/UserDetails.jsx
+++ src/components/onboarding/UserDetails.jsx
@@ -1,37 +1,53 @@
-import React from "react";
-import { doc, collection, setDoc } from "firebase/firestore";
-import { db } from "../../firebase";
+import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { doc, collection, setDoc } from "firebase/firestore";
+import { auth, db } from "../../firebase";
+import { AuthContext } from "../auth/AuthContext";
 
 function UserDetails() {
+    const { currentUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const [contactNum, setContactNum] = useState("");
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const user = auth.currentUser;
+ 
+        if (!user) {
+            console.log("No user is currently signed in");
+            return;
+        }
+
+        const docRef = doc(collection(db, "Users"), currentUser.uid);
+
+        try {
+            await setDoc(docRef, { contactNum }, { merge: true });
+            navigate("/dashboard");
+        } catch (error) {
+            alert("Error saving city: ", error);
+        }
+    };
+
     return (
-        <div className="userDetailsContainer">
-            <h1>Complete profile:</h1>
-            <form>
-                <label htmlFor="companyName"></label>
-                <input
-                    type="text"
-                    id="companyName"
-                    name="companyName"
-                    placeholder="Company Name"
-                />
-                <label htmlFor="phoneNumber"></label>
-                <input
-                    type="number"
-                    id="contact"
-                    name="contact"
-                    placeholder="Contact Number"
-                />
-                <label htmlFor="companyName"></label>
-                <input
-                    type="text"
-                    id="city"
-                    name="city"
-                    placeholder="City"
-                />
-                <button type="submit">Save</button>
-            </form>
-        </div>
+        <>
+            <div className="userDetailsContainer">
+                <h1>Contact Information:</h1>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="phoneNumber"></label>
+                    <input
+                        type="number"
+                        id="contactNum"
+                        name="contactNum"
+                        placeholder="Contact Number"
+                        value={contactNum}
+                        onChange={(e) => setContactNum(e.target.value)}
+                    />
+                    <button type="submit">Save</button>
+                </form>
+            </div>
+            <p>Page 3/3</p>
+        </>
     );
 }
 
