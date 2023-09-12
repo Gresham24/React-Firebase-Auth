61468db7b76a6a7951657b6f865607d3c7087cd4
Gresham24 <tembogresham@gmail.com>
2023-09-13 01:10:50 +0200

    adapted for fetching firestore data

--- src/components/auth/AuthContext.jsx
+++ src/components/auth/AuthContext.jsx
@@ -1,16 +1,46 @@
 import React, { createContext, useState, useEffect } from "react";
-import { auth } from "../../firebase";
+import { auth, db } from "../../firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
 
 export const AuthContext = createContext();
 
-export const AuthProvider = ({ children }) => {
+const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
 
     useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((user) => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
             setCurrentUser(user);
-            // Log the current user object to the console
+
+            // Define the async function to fetch user data from Firestore
+            const fetchUserData = async () => {
+                try {
+                    const docRef = doc(db, "Users", user.uid);
+                    const docSnap = await getDoc(docRef);
+
+                    if (docSnap.exists()) {
+                        console.log(
+                            "User data from Firestore:",
+                            docSnap.data()
+                        );
+                        setCurrentUser({
+                            ...user,
+                            details: docSnap.data(),
+                        });
+                    } else {
+                        console.log(
+                            "Document doesn't exist for UID:",
+                            user.uid
+                        );
+                    }
+                } catch (error) {
+                    console.error("Error fetching user data: ", error);
+                }
+            };
+
             if (user) {
+                fetchUserData();
+
                 console.log("User state: signed in as", user.email);
             } else {
                 console.log("User state: signed out");
