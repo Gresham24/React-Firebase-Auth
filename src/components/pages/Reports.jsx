bea92709d684de1ef12a32dd8e7fe4319b5cd25f
Gresham24 <tembogresham@gmail.com>
2023-09-13 01:11:00 +0200

    pulling firestore data for report

--- src/components/pages/Reports.jsx
+++ src/components/pages/Reports.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from "../auth/AuthContext";
 
-function Reports() {
+const Reports = () => {
+  const { currentUser } = useContext(AuthContext);
+
   return (
       <div className="reportsContainer">
+        <h1>Reports</h1>
+          {currentUser ? (
+    currentUser.details ? (
+              <div>
+                  <p><strong>City:</strong> {currentUser.details.city}</p>
+                  <p><strong>Company Name:</strong> {currentUser.details.companyName}</p>
+                  <p><strong>Contact Number:</strong> {currentUser.details.contactNum}</p>
+                  <p><strong>Email:</strong> {currentUser.details.email}</p>
+                  <p><strong>First Name:</strong> {currentUser.details.firstName}</p>
+                  <p><strong>Last Name:</strong> {currentUser.details.lastName}</p>
+              </div>
+              ) : (
+        <p>Loading user details...</p>
+    )
+) : (
+    <p>No user logged in.</p>
+          )}
           {/* fYR9AGQ3rwYZb7b7dbhvfFdEGKq2
            */}
-          <h1>Reports</h1>
+          
       </div>
   );
 }
