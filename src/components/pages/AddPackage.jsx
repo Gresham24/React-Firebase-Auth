9e00429aee01443271e1f82a36389300252be24a
Gresham24 <tembogresham@gmail.com>
2023-09-20 21:07:11 +0200

    promo package to display end date logic

--- src/components/pages/AddPackage.jsx
+++ src/components/pages/AddPackage.jsx
@@ -3,9 +3,8 @@ import Sidebar from "./Sidebar";
 import { Link } from "react-router-dom";
 import { doc, collection, setDoc } from "firebase/firestore";
 
-
 export function AddPackage() {
-    const [PackageType, setPackageType] = useState();
+    const [PackageType, setPackageType] = useState("");
     const [PackageName, setPackageName] = useState("");
     const [PackagePrice, setPackagePrice] = useState("");
     const [PackageStartDate, setPackageStartDate] = useState("");
@@ -14,13 +13,28 @@ export function AddPackage() {
     const handleChange = (e) => {
         setPackageType(e.target.value);
     };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const PackageName = e.target.PackageName.value;
+        const PackagePrice = e.target.PackagePrice.value;
+        const StartDate = e.target.StartDate.value;
+        const EndDate = e.target.EndDate ? e.target.EndDate.value : null;
+
+        // Add to Firestore (or any other logic you want)
+    };
+
+    // e.target.reset()
+
+    
     return (
         <div className="page">
             <Sidebar />
             <div className="addPackagesContainer">
                 <h1>Add Package</h1>
                 <div className="addPackageFormContainer">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <label htmlFor="packageName">Package Name</label>
                         <input
                             type="text"
@@ -44,7 +58,7 @@ export function AddPackage() {
                             required
                         >
                             <option value=" ">Choose a package</option>
-                            <option value="standPackage">Standard</option>
+                            <option value="StandPackage">Standard</option>
                             <option value="PromoPackage">Promotion</option>
                         </select>
 
@@ -56,13 +70,21 @@ export function AddPackage() {
                             required
                         />
 
-                        <label htmlFor="endDate">End Date</label>
-                        <input
-                            type="date"
-                            id="endDate"
-                            name="endDate"
-                            required
-                        />
+                        {PackageType === "PromoPackage" && (
+                            <>
+                                <label htmlFor="endDate">End Date</label>
+                                <input
+                                    type="date"
+                                    id="endDate"
+                                    name="endDate"
+                                    required={
+                                        PackageType === "PromoPackage"
+                                            ? true
+                                            : false
+                                    }
+                                />
+                            </>
+                        )}
                     </form>
                     <div className="footerButtonWrapper">
                         <Link to="/packages">Cancel</Link>
