b9ed5ed712f651405e4ba5338356a50e6ab59fe5
Gresham24 <tembogresham@gmail.com>
2023-10-04 22:45:49 +0200

    created Landing stylesheet

--- src/components/pages/Landing.jsx
+++ src/components/pages/Landing.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Landing.css"
 
 function Landing() {
     return (
-        <>
+        <div className="container">
             <h1>Landing Page</h1>
-            <Link to="/registration">Register Account</Link>
-            <br></br>
-            <Link to="/SignIn">Sign in</Link>
-        </>
+            <div>
+                <Link to="/registration">Register Account</Link>
+                <br></br>
+                <Link to="/SignIn">Sign in</Link>
+            </div>
+        </div>
     );
 }
 
